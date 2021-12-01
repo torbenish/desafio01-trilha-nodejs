6168f19dd574c2888f2c3ffd53973896b624e92f
torbenish <ishiitorben@gmail.com>
2021-12-01 15:58:05 -0300

    Test on Ubuntu

--- src/index.js
+++ src/index.js
@@ -69,7 +69,13 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  user.title = title;
+  user.deadline = deadline;
+  
+  return response.status(201).json()
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
